f5311e0d3f88e8872f20e8be7d1740658709975c
Nour-ibrahem <nouribrahem207@gmail.com>
2026-09-05 14:19:18 +0300

    Fix Production Issu Part Three

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,9 +1,6 @@
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
 
-// Use Node.js runtime instead of Edge to be compatible with Vercel Services
-export const runtime = 'nodejs';
-
 export default createMiddleware(routing);
 
 export const config = {
